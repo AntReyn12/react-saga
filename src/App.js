69e701aee826be8ecdff17777c0aa50e76320c86
ant_reyn <antreyn@udel.edu>
2021-07-28 15:26:09 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useSelector} from "react-redux";
+import "./App.css";
+import Counter from "./Counter";
 
-function App() {
+const App = () => {
+  const count = useSelector((state) => state.counter.count);
+  const voters = [
+    "Anthony Reynolds",
+    "Ben Much",
+    "Andrew Stover",
+    "Chris Alvarez",
+  ];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Redux made easy!</h1>
+        <h3>Total votes: {count}</h3>
+        {voters.map((voter) => (
+          <Counter name={voter} />
+        ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
