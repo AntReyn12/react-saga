e15ef5f3a379f5ad32e70fa789541a8230c36072
ant_reyn <antreyn@udel.edu>
2021-07-29 15:45:05 -0400

    got rid of the counter component and only kept the user component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from "react";
 import {useSelector, useDispatch} from "react-redux";
 import "./App.css";
-import Counter from "./Counter";
 import {getUser} from "./redux/ducks/user";
 
 const App = () => {
@@ -14,13 +13,6 @@ const App = () => {
   const user = useSelector((state) => state.user.user);
   console.log(user);
 
-  const count = useSelector((state) => state.counter.count);
-  const voters = [
-    "Anthony Reynolds",
-    "Ben Much",
-    "Andrew Stover",
-    "Chris Alvarez",
-  ];
   return (
     <div className="App">
       <header className="App-header">
@@ -29,11 +21,6 @@ const App = () => {
             Hello {user.firstName} {user.lastName}. Welcome!
           </h1>
         )}
-        <h1>Redux made easy!</h1>
-        <h3>Total votes: {count}</h3>
-        {voters.map((voter) => (
-          <Counter name={voter} />
-        ))}
       </header>
     </div>
   );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,9 @@
 import {combineReducers, createStore, applyMiddleware} from "redux";
 import createSagaMiddleware from "redux-saga";
-import counterReducer from "./ducks/counter";
 import userReducer from "./ducks/user";
 import {watcherSaga} from "./sagas/rootSaga";
 
 const reducer = combineReducers({
-  counter: counterReducer,
   user: userReducer,
 });
 
